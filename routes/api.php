608193090f95ed5f52acd570eb037671e31ba9d5
Deisi <deisi.bonilla@gmail.com>
2021-01-09 19:57:46 +0100

    :sparkles: Feature: added login page & controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +17,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('login','UserController@login');
+Route::get('profile','UserController@getAuthenticatedUser');
+
+Route::middleware('auth:api')->get('/user', function(Request $request){
     return $request->user();
 });
