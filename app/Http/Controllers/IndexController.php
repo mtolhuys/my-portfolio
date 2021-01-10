f8ee711f58cc5372ec4ee0eec656663e377796c8
Deisi <deisi.bonilla@gmail.com>
2021-01-10 01:25:01 +0100

    :hammer_and_wrench: Refactored: added mock data

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 
 class IndexController extends Controller
@@ -12,13 +14,22 @@ class IndexController extends Controller
     }
 
     /**
-     * @return array
+     * @return array|false|string
      */
-    public function instaFeed(): array
+    public function instaFeed()
     {
-        return Http::get('https://graph.instagram.com/me/media', [
-            'access_token' => config('services.instagram.token'),
-            'fields' => \request('fields', 'media_url,media_type,caption,permalink'),
-        ])->json();
+        try {
+            if (app()->environment('production')) {
+                return Http::get('https://graph.instagram.com/me/media', [
+                    'access_token' => config('services.instagram.token'),
+                    'fields' => \request('fields', 'media_url,media_type,caption,permalink'),
+                ])->json();
+            } else {
+                return File::get(base_path('tests/Mocks/insta-feed.json'));
+            }
+        } catch (RequestException $e) {
+            //TODO: Log this when it happens!
+            return [];
+        }
     }
 }
