2be68a0e9fe007add670e26d9938a703a894f554
Deisi <deisi.bonilla@gmail.com>
2021-01-15 18:36:21 +0100

    :hammer_and_wrench: Refactored: added editor component & login request

---
+++ resources/js/components/Pages/UserPages/Editor/Editor.vue
@@ -0,0 +1,55 @@
+<template>
+    <section>
+        <vue-editor v-model="content"></vue-editor>
+    </section>
+</template>
+
+<script>
+import { VueEditor } from "vue2-editor";
+import axios from "axios";
+export default {
+    components: {
+        VueEditor
+    },
+
+    data() {
+        return {
+            content: "<h1>Html For Editor</h1>",
+            customToolbar:
+                [["bold", "italic", "underline"],
+                [{ list: "ordered" }, { list: "bullet" }],
+                ["image", "code-block"]]
+        };
+    },
+
+    methods: {
+        handleImageAdded: function(file, Editor, cursorLocation, resetUploader) {
+            // An example of using FormData
+            // NOTE: Your key could be different such as:
+            // formData.append('file', file)
+
+            let formData = new FormData();
+            formData.append("image", file);
+
+            axios({
+                url: "https://fakeapi.yoursite.com/images",
+                method: "POST",
+                data: formData
+            })
+                .then(result => {
+                    let url = result.data.url; // Get url from response
+                    Editor.insertEmbed(cursorLocation, "image", url);
+                    resetUploader();
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        },
+        handleSavingContent: function() {
+            // You have the content to save
+            console.log(this.content);
+        }
+    }
+};
+</script>
+
