72d5a6491eeee73e948f749972aa2abecd68f0c9
Deisi <deisi.bonilla@gmail.com>
2021-02-06 20:07:17 +0100

    :sparkles: Feature: added migration files & contact models

---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Contact;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var
+     */
+    public $contact;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param Contact $contact
+     */
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mail.contact');
+    }
+}
